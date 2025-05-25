57b1adcb3b818faafaa2035d8605ff88904f7d56
SADMANGaming <74666591+SADMANGaming@users.noreply.github.com>
2025-05-25 18:55:03 +0600

    Add files via upload

---
+++ bin_functions.hpp
@@ -0,0 +1,123 @@
+#ifndef _BIN_FUNCTIONS_HPP_
+#define _BIN_FUNCTIONS_HPP_
+
+// Cvar
+typedef cvar_t* (*Cvar_Get_t)(const char *var_name, const char *var_value, unsigned short flags);
+static const Cvar_Get_t Cvar_Get = (Cvar_Get_t)0x08072a7c;
+
+typedef cvar_t* (*Cvar_FindVar_t)(const char *var_name);
+static const Cvar_FindVar_t Cvar_FindVar = (Cvar_FindVar_t)0x08072916;
+
+// Cmd
+typedef char* (*Cmd_Argv_t)(int arg);
+static const Cmd_Argv_t Cmd_Argv = (Cmd_Argv_t)0x080600f4;
+
+typedef int (*Cmd_Argc_t)();
+static const Cmd_Argc_t Cmd_Argc = (Cmd_Argc_t)0x080600ea;
+
+typedef int (*Cmd_FollowCycle_f_t)(gentity_s *ent, int dir);
+
+typedef void (*Cmd_TokenizeString_t)(const char *text_in);
+static const Cmd_TokenizeString_t Cmd_TokenizeString = (Cmd_TokenizeString_t)0x08060423;
+
+// Com
+typedef void (*Com_DPrintf_t)(const char *format, ...);
+static const Com_DPrintf_t Com_DPrintf = (Com_DPrintf_t)0x0806fc5f;
+
+typedef void (*Com_PrintMessage_t)(int channel, const char *message);
+static const Com_PrintMessage_t Com_PrintMessage = (Com_PrintMessage_t)0x0806f827;
+
+typedef void (*Com_Printf_t)(const char *format, ...);
+static const Com_Printf_t Com_Printf = (Com_Printf_t)0x0806fc10;
+
+typedef void (*Com_Error_t)(errorParm_t code, const char *format, ...);
+static const Com_Error_t Com_Error = (Com_Error_t)0x0806fe74;
+
+//// PB
+typedef const char * (*PBAuthClient_t)(const char *clientAddress, int cl_punkbuster, const char *PBGuid);
+static const PBAuthClient_t PBAuthClient = (PBAuthClient_t)0x080c191c;
+
+typedef void (*PbSvAddEvent_t)(int event, int clientNum, int offset, byte *msg);
+static const PbSvAddEvent_t PbSvAddEvent = (PbSvAddEvent_t)0x080C174C;
+
+typedef void (*PbPassConnectString_t)(const char *addr, byte *data);
+static const PbPassConnectString_t PbPassConnectString = (PbPassConnectString_t)0x080C18EC;
+
+
+// SL
+typedef char* (*SL_ConvertToString_t)(unsigned int index);
+static const SL_ConvertToString_t SL_ConvertToString = (SL_ConvertToString_t)0x080a3460; //done
+
+// Scr
+typedef unsigned short (*Scr_AllocArray_t)(void);
+static const Scr_AllocArray_t Scr_AllocArray = (Scr_AllocArray_t)0x080a5ab4;
+
+// SV
+typedef playerState_t* (*SV_GameClientNum_t)(int num);
+static const SV_GameClientNum_t SV_GameClientNum = (SV_GameClientNum_t)0x0808d331;
+
+typedef gentity_t* (*SV_ClientThink_t)(client_t *cl, usercmd_t *cmd);
+static const SV_ClientThink_t SV_ClientThink = (SV_ClientThink_t)0x0808ca58;
+
+typedef void (*SV_FreeClient_t)(client_t *client);
+static const SV_FreeClient_t SV_FreeClient = (SV_FreeClient_t)0x08089daf;
+
+typedef gentity_t * (*SV_GentityNum_t)(int num);
+static const SV_GentityNum_t SV_GentityNum = (SV_GentityNum_t)0x0808d314;
+
+typedef void (*SV_UserinfoChanged_t)(client_t *client);
+static const SV_UserinfoChanged_t SV_UserinfoChanged = (SV_UserinfoChanged_t)0x0808c4fd;
+
+typedef void (*SV_DropClient_t)(client_t *drop, const char *reason);
+static const SV_DropClient_t SV_DropClient = (SV_DropClient_t)0x0808ac11;
+
+typedef void (*SV_DirectConnect_t)(netadr_t from);
+static const SV_DirectConnect_t SV_DirectConnect = (SV_DirectConnect_t)0x8089E7E;
+
+typedef void (QDECL *SV_SendServerCommand_t)(client_t *cl, int type, const char *fmt, ...);
+static const SV_SendServerCommand_t SV_SendServerCommand = (SV_SendServerCommand_t)0x80920DE;
+
+// SVC
+typedef void (*SVC_Status_t)(netadr_t from);
+static const SVC_Status_t SVC_Status = (SVC_Status_t)0x0809246E;
+
+typedef void (*SVC_Info_t)(netadr_t from);
+static const SVC_Info_t SVC_Info = (SVC_Info_t)0x08092A74;
+
+typedef void (*SVC_RemoteCommand_t)(netadr_t from, msg_t *msg);
+static const SVC_RemoteCommand_t SVC_RemoteCommand = (SVC_RemoteCommand_t)0x080930D0;
+
+//Info
+typedef char* (*Info_ValueForKey_t)(const char *s, const char *key);
+static const Info_ValueForKey_t Info_ValueForKey = (Info_ValueForKey_t)0x8086397;
+
+// NET
+typedef const char * (*NET_AdrToString_t)(netadr_t a);
+static const NET_AdrToString_t NET_AdrToString = (NET_AdrToString_t)0x08083e10;
+
+typedef int (*NET_CompareBaseAdr_t)(netadr_t a,netadr_t b);
+static const NET_CompareBaseAdr_t NET_CompareBaseAdr = (NET_CompareBaseAdr_t)0x08083de3;
+
+typedef qboolean (*NET_IsLocalAddress_t)(netadr_t from);
+static const NET_IsLocalAddress_t NET_IsLocalAddress = (NET_IsLocalAddress_t)0x0808403c;
+
+typedef void (*NET_OutOfBandPrint_t)(netsrc_t net_socket, netadr_t adr, const char *format, ...);
+static const NET_OutOfBandPrint_t NET_OutOfBandPrint = (NET_OutOfBandPrint_t)0x0808428e;
+
+typedef qboolean (*NET_CompareAdr_t)(netadr_t a, netadr_t b);
+static const NET_CompareAdr_t NET_CompareAdr = (NET_CompareAdr_t)0x0808400f;
+
+typedef void (*NetadrToSockadr_t)(netadr_t *a, struct sockaddr_in *s);
+static const NetadrToSockadr_t NetadrToSockadr = (NetadrToSockadr_t)0x080d4a0c;
+
+typedef qboolean (*NET_StringToAdr_t)(const char *s, netadr_t *a);
+static const NET_StringToAdr_t NET_StringToAdr = (NET_StringToAdr_t)0x080844e0;
+
+typedef const char* (*NET_idkPrint_t)(const char *format, ...);
+static const NET_idkPrint_t NET_idkPrint = (NET_idkPrint_t)0x806FC10;
+
+//Cmd
+typedef void (*Cmd_AddCommand_t)(const char *cmd_name, xcommand_t function);
+static const Cmd_AddCommand_t Cmd_AddCommand = (Cmd_AddCommand_t)0x806043E; //txt str
+
+#endif
---
+++ gsc.cpp
@@ -0,0 +1,380 @@
+#include "gsc.hpp"
+
+#include <stdint.h>
+#include <sys/time.h>
+
+scr_function_t scriptFunctions[] =
+{
+
+    {"testFunction", gsc_testfunction, 0},
+    {"getsubstr", gsc_utils_getsubstr, 0},
+    {"strtok", gsc_utils_strtok, 0},
+    {"sendCommandToClient", gsc_utils_sendcommandtoclient, 0},
+
+
+    {NULL, NULL, 0} // Terminator
+};
+
+xfunction_t Scr_GetCustomFunction(const char **fname, int *fdev)
+{
+    xfunction_t m = Scr_GetFunction(fname, fdev);
+    if(m)
+        return m;
+
+    for (int i = 0; scriptFunctions[i].name; i++)
+    {
+        if(strcasecmp(*fname, scriptFunctions[i].name))
+            continue;
+
+        scr_function_t func = scriptFunctions[i];
+        *fname = func.name;
+        *fdev = func.developer;
+        return func.call;
+    }
+    return NULL;
+}
+
+
+scr_method_t scriptMethods[] =
+{
+    {"setVelocity", gsc_player_setvelocity, 0},
+    {"getVelocity", gsc_player_getvelocity, 0},
+    {"setGravity", gsc_player_setgravity, 0},
+    {"setSpeed", gsc_player_setspeed, 0},
+    {"connectionlesspackettoclient", gsc_player_connectionlesspackettoclient, 0},
+    {"getPing", gsc_player_getping, 0},
+    {"dropclient", gsc_player_dropclient, 0},
+    {"processClientCommand", gsc_player_processclientcommand, 0},
+    {"sendservercommand", gsc_player_sendservercommand, 0},
+
+    // Bot
+    {"setUse", gsc_bots_setuse, 0},
+    {"setWalkValues", gsc_bots_setwalkvalues, 0},
+    {"setWalkDir", gsc_bots_setwalkdir, 0},
+    {"setBotStance", gsc_bots_setbotstance, 0},
+    {"setLean", gsc_bots_setlean, 0},
+    {"setAim", gsc_bots_setaim, 0},
+    {"fireWeapon", gsc_bots_fireweapon, 0},
+    {"meleeWeapon", gsc_bots_meleeweapon, 0},
+    {"reloadWeapon", gsc_bots_reloadweapon, 0},
+    {"switchToWeaponId", gsc_bots_switchtoweaponid, 0},
+
+    {"aimButtonPressed", gsc_player_button_ads, 0},
+    {"leftButtonPressed", gsc_player_button_left, 0},
+    {"rightButtonPressed", gsc_player_button_right, 0},
+    {"forwardButtonPressed", gsc_player_button_forward, 0},
+    {"backButtonPressed", gsc_player_button_back, 0},
+    {"jumpButtonPressed", gsc_player_button_jump, 0},
+    {"leanleftButtonPressed", gsc_player_button_leanleft, 0},
+    {"leanRightButtonPressed", gsc_player_button_leanright, 0},
+    {"reloadButtonPressed", gsc_player_button_reload, 0},
+
+    {"testMethod", gsc_testmethod, 0},
+    {NULL, NULL, 0} // Terminator
+};
+
+xmethod_t Scr_GetCustomMethod(const char **fname, qboolean *fdev)
+{
+    xmethod_t m = Scr_GetMethod(fname, fdev);
+    if(m)
+        return m;
+
+    for (int i = 0; scriptMethods[i].name; i++)
+    {
+        if(strcasecmp(*fname, scriptMethods[i].name))
+            continue;
+        
+        scr_method_t func = scriptMethods[i];
+        *fname = func.name;
+        *fdev = func.developer;
+        return func.call;
+    }
+    return NULL;
+}
+
+void stackError(const char *format, ...)
+{
+    char s[MAX_STRINGLENGTH];
+    int len = 0;
+    va_list va;
+
+    va_start(va, format);
+    Q_vsnprintf(s, sizeof(s) - 1, format, va);
+    va_end(va);
+
+    len = strlen(s);
+    s[len] = '\n';
+    s[len + 1] = '\0';
+    Com_PrintMessage(0, s);
+    //Scr_CodeCallback_Error(qfalse, qfalse, "stackError", s);
+    Scr_Error(s); //TODO: look about calling Scr_CodeCallback_Error instead
+}
+
+int stackGetParams(const char *params, ...)
+{
+    va_list args;
+    va_start(args, params);
+
+    int errors = 0;
+
+    for ( size_t i = 0; i < strlen(params); i++ )
+    {
+        switch ( params[i] )
+        {
+        case ' ': // Ignore param
+            break;
+
+        case 'i':
+        {
+            int *tmp = va_arg(args, int *);
+            if ( !stackGetParamInt(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not an int\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        case 'v':
+        {
+            float *tmp = va_arg(args, float *);
+            if ( !stackGetParamVector(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not a vector\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        case 'f':
+        {
+            float *tmp = va_arg(args, float *);
+            if ( ! stackGetParamFloat(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not a float\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        case 's':
+        {
+            char **tmp = va_arg(args, char **);
+            if ( !stackGetParamString(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not a string\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        case 'c':
+        {
+            unsigned int *tmp = va_arg(args, unsigned int *);
+            if ( !stackGetParamConstString(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not a const string\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        case 'l':
+        {
+            char **tmp = va_arg(args, char **);
+            if ( !stackGetParamLocalizedString(i, tmp) )
+            {
+                Com_DPrintf("\nstackGetParams() Param %i is not a localized string\n", i);
+                errors++;
+            }
+            break;
+        }
+
+        default:
+            errors++;
+            Com_DPrintf("\nUnknown identifier [%c] passed to stackGetParams()\n", params[i]);
+            break;
+        }
+    }
+
+    va_end(args);
+    return errors == 0; // success if no errors
+}
+
+int stackGetParamInt(int param, int *value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type == STACK_FLOAT )
+    {
+        *value = var->u.floatValue;
+        return 1;
+    }
+
+    if ( var->type != STACK_INT )
+        return 0;
+
+    *value = var->u.intValue;
+
+    return 1;
+}
+
+
+int stackGetParamString(int param, char **value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type != STACK_STRING )
+        return 0;
+
+    *value = SL_ConvertToString(var->u.stringValue);
+
+    return 1;
+}
+
+int stackGetParamConstString(int param, unsigned int *value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type != STACK_STRING )
+        return 0;
+
+    *value = var->u.stringValue;
+
+    return 1;
+}
+
+int stackGetParamLocalizedString(int param, char **value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type != STACK_LOCALIZED_STRING )
+        return 0;
+
+    *value = SL_ConvertToString(var->u.stringValue);
+
+    return 1;
+}
+
+int stackGetParamVector(int param, vec3_t value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type != STACK_VECTOR )
+        return 0;
+
+    VectorCopy(var->u.vectorValue, value);
+
+    return 1;
+}
+
+int stackGetParamFloat(int param, float *value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type == STACK_INT )
+    {
+        *value = var->u.intValue;
+        return 1;
+    }
+
+    if ( var->type != STACK_FLOAT )
+        return 0;
+
+    *value = var->u.floatValue;
+
+    return 1;
+}
+
+int stackGetParamObject(int param, unsigned int *value)
+{
+    if ( param >= Scr_GetNumParam() )
+        return 0;
+
+    VariableValue *var;
+    var = &scrVmPub.top[-param];
+
+    if ( var->type != STACK_OBJECT )
+        return 0;
+
+    *value = var->u.pointerValue;
+
+    return 1;
+}
+
+/**
+ * @brief Base time in seconds
+ */
+time_t sys_timeBase = 0;
+
+/**
+ * @brief Current time in ms, using sys_timeBase as origin
+ */
+uint64_t Sys_Milliseconds64(void)
+{
+    struct timeval tp;
+
+    gettimeofday(&tp, NULL);
+
+    if ( !sys_timeBase )
+    {
+        sys_timeBase = tp.tv_sec;
+        return tp.tv_usec / 1000;
+    }
+
+    return (tp.tv_sec - sys_timeBase) * 1000 + tp.tv_usec / 1000;
+}
+
+
+// For tests
+void gsc_testfunction()
+{
+    printf("##### gsc_testfunction\n");
+}
+void gsc_testmethod(scr_entref_t ref)
+{
+    printf("##### gsc_testmethod\n");
+
+    int id = ref.entnum;
+
+    if ( id >= MAX_CLIENTS )
+    {
+        stackError("gsc_player_testcommand() entity %i is not a player", id);
+        stackPushUndefined();
+        return;
+    }
+
+    //client_t* client = &svs.clients[id];
+    //gentity_t* gentity = &g_entities[id];
+    //playerState_t *ps = SV_GameClientNum(id);
+    //gclient_t *gclient = gentity->client;
+
+
+    //printf("##### ps->gravity = %i\n", ps->gravity);
+    //printf("##### ps->viewangles[0] = %f, ps->viewangles[1] = %f, ps->viewangles[2] = %f\n", ps->viewangles[0], ps->viewangles[1], ps->viewangles[2]);
+    
+}
