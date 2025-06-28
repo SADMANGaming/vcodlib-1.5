cf2c652a2be6c311c3722ecf0a17a9acd6c1bff8
SADMANGaming <74666591+SADMANGaming@users.noreply.github.com>
2025-06-28 14:27:30 +0600

    improved gsc methods added by Prawy

--- src/gsc_utils.hpp
+++ src/gsc_utils.hpp
@@ -3,6 +3,13 @@
 
 #include "gsc.hpp"
 
+void gsc_utils_file_exists();
+void gsc_utils_fopen();
+void gsc_utils_fread();
+void gsc_utils_fwrite();
+void gsc_utils_fclose();
+void gsc_utils_system();
+
 void gsc_utils_getsubstr();
 void gsc_utils_strtok(); // From cod2rev
 void gsc_utils_sendcommandtoclient();
